ed25171c862e9917159637aaf4b94ffb0364b52c
Roshan Kharel <rosn.khrl@gmail.com>
2020-03-06 22:37:53 +1100

    assessment item 1 task 1 - code refactored

--- 1 assessmest/src/main/ABCBank.java
+++ 1 assessmest/src/main/ABCBank.java
@@ -21,9 +21,9 @@ public class ABCBank {
 
 		System.out.printf(
 				"\n" + horizontalDashedLine + "\n\n" + centerText("ABCBank", 42) + "\n\n"
-				+ centerText("Return on Bank Term Deposits", 42) + "\n\n" + horizontalDashedLine + "\n\n" + "Client: %s\n"
-				+ "Deposit Amount: %.2f\n" + "Term: %s month" + (terms > 1 ? "s" : "") + "\n"
-				+ "Interest Earned: $%.2f\n" + "Final Balance: $%.2f\n\n" + horizontalDashedLine,
+						+ centerText("Return on Bank Term Deposits", 42) + "\n\n" + horizontalDashedLine + "\n\n"
+						+ "Client: %s\n" + "Deposit Amount: $%.2f\n" + "Term: %s Month" + (terms > 1 ? "s" : "") + "\n"
+						+ "Interest Earned: $%.2f\n" + "Final Balance: $%.2f\n\n" + horizontalDashedLine,
 				clientName, depositAmount, terms, interest, totalBalance);
 
 		return this;
@@ -48,11 +48,15 @@ public class ABCBank {
 
 	public void ready() {
 		while (true) {
+			System.out.println();
+
 			run();
 
 			System.out.print("\n\nCalculate interest for another client? [Yes / No (any key)]: ");
+
 			String keepGoing = keyboard.nextLine().strip().toLowerCase();
-			System.out.println("\n");
+
+			System.out.println();
 
 			if (keepGoing.equals("yes") || keepGoing.equals("y"))
 				continue;
@@ -64,57 +68,57 @@ public class ABCBank {
 	}
 
 	public void run() {
-		String clientName;
-		double depositAmount;
-		int terms;
+		String clientName = askClientName();
+		double depositAmount = askDepositeAmount();
+		int terms = askNumberOfTerms();
 
+		deposit(clientName, terms, depositAmount);
+	}
+
+	protected String askClientName() {
 		do {
 			System.out.print("Enter client's name: ");
-			clientName = keyboard.nextLine();
+			String clientName = keyboard.nextLine();
 
-			if (clientName.strip().length() > 0) {
-				break;
-			}
+			if (clientName.strip().length() > 0)
+				return clientName;
 
 			System.out.println("Error: Client's name should at least one character.");
-
 		} while (true);
+	}
 
+	protected double askDepositeAmount() {
 		do {
 			System.out.print("Enter deposit amount: ");
-			String depositAmountStr = keyboard.nextLine();
 
 			try {
-				depositAmount = Double.valueOf(depositAmountStr);
+				double depositAmount = Double.valueOf(keyboard.nextLine());
 
-				if (depositAmount > 0) {
-					break;
-				}
+				if (depositAmount > 0)
+					return depositAmount;
 
 				System.out.println("Error: Deposite amount should be a greater than 0 (zero).");
 			} catch (NumberFormatException e) {
 				System.out.println("Error: Deposite amount should be a valid number.");
 			}
 		} while (true);
+	}
 
+	protected int askNumberOfTerms() {
 		do {
 			System.out.print("Enter terms (in months): ");
-			String termsStr = keyboard.nextLine();
 
 			try {
-				terms = Integer.valueOf(termsStr);
+				int terms = Integer.valueOf(keyboard.nextLine());
 
-				if (terms > 0) {
-					break;
-				}
+				if (terms > 0)
+					return terms;
 
 				System.out.println("Error: Terms must be greater than 0 (zero).");
 			} catch (NumberFormatException e) {
 				System.out.println("Error: Terms must be a valid integer.");
 			}
 		} while (true);
-
-		deposit(clientName, terms, depositAmount);
 	}
 
 	protected String centerText(String text, int width) {
