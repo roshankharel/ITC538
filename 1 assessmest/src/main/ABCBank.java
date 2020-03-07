8681cbb84550c18464e0dd1ae9702731723c486f
Roshan Kharel <rosn.khrl@gmail.com>
2020-03-07 11:23:28 +1100

    A1T1 code refactor and documentation

--- 1 assessmest/src/main/ABCBank.java
+++ 1 assessmest/src/main/ABCBank.java
@@ -2,6 +2,14 @@ package main;
 
 import java.util.Scanner;
 
+/**
+* The class provides a command line interface to ABCBank employees
+* to calculate interest and total balance of a client for 
+* a given deposit in fixed terms
+* 
+* @version 0.1
+* @author Roshan Kharel, 11691041
+*/
 public class ABCBank {
 	protected Scanner keyboard;
 
@@ -13,45 +21,66 @@ public class ABCBank {
 		new ABCBank().ready();
 	}
 
+	/**
+    * Calculate and display interest earned, initial deposit amount,
+    * final balance, and other details
+    *
+    * @param  clientName		the name of the client
+    * @param  terms				number of months to deposit
+    * @param  depositAmount		client's deposit amount
+    * @return					ABCBank instance (for method chaining)
+    */
 	public ABCBank deposit(String clientName, int terms, double depositAmount) {
 		double interest = calculateInterest(depositAmount, terms);
 		String horizontalDashedLine = "-".repeat(42);
 
-		double totalBalance = depositAmount + interest;
+		double finalBalance = depositAmount + interest;
 
 		System.out.printf(
-					"\n" + horizontalDashedLine + "\n\n"
-					+ centerText("ABCBank", 42) + "\n\n"
-					+ centerText("Return on Bank Term Deposits", 42) + "\n\n"
-					+ horizontalDashedLine + "\n\n"
-					+ "Client: %s\n"
-					+ "Deposit Amount: $%.2f\n"
-					+ "Term: %s Month" + (terms > 1 ? "s" : "") + "\n"
-					+ "Interest Earned: $%.2f\n"
-					+ "Final Balance: $%.2f\n\n"
-					+ horizontalDashedLine,
-				clientName, depositAmount, terms, interest, totalBalance);
+				"\n" + horizontalDashedLine + "\n\n"
+				+ centerText("ABCBank", 42) + "\n\n"
+				+ centerText("Return on Bank Term Deposits", 42) + "\n\n"
+				+ horizontalDashedLine + "\n\n"
+				+ "Client: %s\n"
+				+ "Deposit Amount: $%.2f\n"
+				+ "Term: %s Month" + (terms > 1 ? "s" : "") + "\n"
+				+ "Interest Earned: $%.2f\n"
+				+ "Final Balance: $%.2f\n\n"
+				+ horizontalDashedLine,
+			clientName, depositAmount, terms, interest, finalBalance);
 
 		return this;
 	}
 
+	/**
+    * Calculate interest that the client can earn at the end of terms of deposit 
+    *
+    * @param  amount	the client's deposit amount
+    * @param  terms		duration (in months) of fixed deposit
+    * @return			interest earned by client
+    */
 	protected double calculateInterest(double amount, int terms) {
 		double interest = 0.0;
+		double years = Double.valueOf(terms) / 12d;
 
-		if (amount < 1000)
-			interest = amount * 0.02;
-		else if (amount > 1000 && amount <= 5000)
+		if (amount < 1000) {
+			interest = amount * 0.02;			
+		} else if (amount > 1000 && amount <= 5000) {
 			interest = amount * 0.025;
-		else if (amount > 5000 && amount <= 10000)
+		} else if (amount > 5000 && amount <= 10000) {
 			interest = amount * 0.03;
-		else if (amount > 10000 && amount <= 20000)
+		} else if (amount > 10000 && amount <= 20000) {
 			interest = amount * 0.035;
-		else
+		} else {
 			interest = amount * 0.04;
-
-		return interest * (Double.valueOf(terms) / 12d);
+		}
+		
+		return interest * years;
 	}
 
+	/**
+    * Method to allow user to calculate multiple clients' interest interactively
+    */
 	public void ready() {
 		while (true) {
 			System.out.println();
@@ -73,6 +102,9 @@ public class ABCBank {
 		keyboard.close();
 	}
 
+	/**
+    * Method to allow user to calculate single client's interest interactively
+    */
 	public void run() {
 		String clientName = askClientName();
 		double depositAmount = askDepositeAmount();
@@ -81,6 +113,11 @@ public class ABCBank {
 		deposit(clientName, terms, depositAmount);
 	}
 
+	/**
+	* Interactively prompts for client's name
+	*
+	* @return Client's name
+	*/
 	protected String askClientName() {
 		do {
 			System.out.print("Enter client's name: ");
@@ -89,10 +126,15 @@ public class ABCBank {
 			if (clientName.strip().length() > 0)
 				return clientName;
 
-			System.out.println("Error: Client's name should at least one character.");
+			System.out.printf("\nError: Client's name should be at least single character.\n");
 		} while (true);
 	}
 
+	/**
+	* Interactively prompts for deposit amount
+	*
+	* @return Client's deposit amount
+	*/
 	protected double askDepositeAmount() {
 		do {
 			System.out.print("Enter deposit amount: ");
@@ -103,13 +145,18 @@ public class ABCBank {
 				if (depositAmount > 0)
 					return depositAmount;
 
-				System.out.println("Error: Deposite amount should be a greater than 0 (zero).");
+				System.out.printf("\nError: Deposite amount should be a greater than 0 (zero).\n");
 			} catch (NumberFormatException e) {
-				System.out.println("Error: Deposite amount should be a valid number.");
+				System.out.printf("\nError: Deposite amount should be a valid number.\n");
 			}
 		} while (true);
 	}
 
+	/**
+	* Interactively prompts for number of terms in months
+	*
+	* @return Number of terms (in month)
+	*/
 	protected int askNumberOfTerms() {
 		do {
 			System.out.print("Enter terms (in months): ");
@@ -120,15 +167,27 @@ public class ABCBank {
 				if (terms > 0)
 					return terms;
 
-				System.out.println("Error: Terms must be greater than 0 (zero).");
+				System.out.printf("\nError: Terms must be greater than 0 (zero).\n");
 			} catch (NumberFormatException e) {
-				System.out.println("Error: Terms must be a valid integer.");
+				System.out.println("\nError: Terms must be a valid integer.\n");
 			}
 		} while (true);
 	}
 
+	/**
+	* Utility method to center a given text by adding whitespace
+	*
+	* @param text	the text to be padded with whitespace
+	* @param width	the length of the characters in which text is to be centered
+	* @return 		text padded with whitespace
+	*/
 	protected String centerText(String text, int width) {
-		return String.format("%-" + width + "s",
-				String.format("%" + (text.length() + (width - text.length()) / 2) + "s", text));
+		return String.format(
+			"%-" + width + "s",
+			String.format(
+				"%" + (text.length() + (width - text.length()) / 2) + "s",
+				text
+			)
+		);
 	}
 }
