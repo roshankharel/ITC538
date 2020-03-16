09a26a7d7fc7b627609e10e7ae9536fe85a062d0
KHAREL Roshan <11691041@students.local>
2020-03-16 11:55:02 +1100

    A1 - T1: minor changes, T2: methods documenation

--- 1 assessmest/src/main/ZooTicket.java
+++ 1 assessmest/src/main/ZooTicket.java
@@ -3,11 +3,34 @@ package main;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+* This class provides a Command Line Interface (CLI) to help 
+* calculate and display the entry charge for each family group
+* and also display the total takings for a sequence of groups
+*
+* @version 0.1
+* @author Roshan Kharel, 11691041
+*/
 public class ZooTicket {
 	protected final int ACCOMPANIED_CHILDREN_COST = 2;
 	protected final int UNACCOMPANIED_CHILDREN_COST = 5;
 	protected final int ADULT_COST = 10;
 	protected final int SENIOR_COST = 8;
+	
+	protected enum CHOICE {
+		ADD_GROUP(1),
+		NO_GROUP(0);
+		
+		private final int choiceCode;
+
+		CHOICE(int i) {
+			choiceCode = i;
+		}
+		
+		public int getCoiceCode() {
+	        return choiceCode;
+	    }
+	};
 	
 	protected int totalCharge = 0;
 	Scanner keyboard;
@@ -20,16 +43,21 @@ public class ZooTicket {
 		keyboard = new Scanner(System.in);
 	}
 	
+	/**
+    * Method to run indefinitely to compute and display group
+    * entry charge and total takings of all groups
+    */
 	public void run() {
 		boolean keepGoing = true;
 		
-		ArrayList<Integer> acceptedValues = new ArrayList<Integer>();
-		acceptedValues.add(0);
-		acceptedValues.add(1);
+		ArrayList<Integer> validChoices = new ArrayList<Integer>();
+		validChoices.add(0); // selection 0 will display total takings for a sequence of groups
+		validChoices.add(1); // selection 1 will calculate and display the entry charge for each family group
 		
 		
 		String horizontalDashedLine = "-".repeat(60);
 		
+		// display welcome message and pricing information
 		System.out.printf(
 				horizontalDashedLine + "\n" +
 				centerText("Welocme to the Zoo", 60) + "\n" +
@@ -49,7 +77,7 @@ public class ZooTicket {
 			try {
 				group = Integer.valueOf(keyboard.nextLine());
 				
-				if(!acceptedValues.contains(group)) {
+				if(!validChoices.contains(group)) {
 					System.out.println("Error: only 1 and 0 are accepted values.\n");
 					continue;
 				}
@@ -77,6 +105,11 @@ public class ZooTicket {
 		
 	}
 	
+	/**
+	* Interactively asks for number of children in a family group
+	*
+	* @return number of children
+	*/
 	protected int askNumberOfChildren() {
 		do {
 			System.out.print("Enter the number of children (age 6–15): ");
@@ -99,6 +132,11 @@ public class ZooTicket {
 		} while(true);
 	}
 	
+	/**
+	* Interactively asks for number of seniors in a family group
+	*
+	* @return number of seniors
+	*/
 	protected int askNumberOfSeniors() {
 		do {
 			System.out.print("Enter the number of seniors (age 60+): ");
@@ -121,6 +159,11 @@ public class ZooTicket {
 		} while(true);
 	}
 	
+	/**
+	* Interactively asks for number of adults in a family group
+	*
+	* @return number of adults
+	*/
 	protected int askNumberOfAdults() {
 		do {
 			System.out.print("Enter the number of adults (age 16–59): ");
@@ -143,11 +186,20 @@ public class ZooTicket {
 		} while(true);
 	}
 	
+	/**
+    * Method to display total takings of all groups
+    */
 	protected void showTotal() {
 		System.out.printf("Total takings: $%d\n", totalCharge);
 	}
 	
-	
+	/**
+    * Computes and displays group's total entry charge
+    * 
+    * @param  childern			number of children
+    * @param  adults            number of adults
+    * @param  seniors			number of seniors
+    */
 	protected void showTotal(int children, int adults, int seniors) {
 		int numberOfAccompaniedChildren = getNumberOfAccompaniedChildren(children, adults, seniors);
 		int numberOfUnaccompaniedChildren = getNumberOfUnaccompaniedChildren(children, adults, seniors);
@@ -164,26 +216,77 @@ public class ZooTicket {
 		System.out.printf("Total entry charge is $%d\n\n", groupCharge);
 	}
 	
+	/**
+	* Calculates the costs of accompanied children
+	*
+	* @param  count			number of children
+	*
+	* @return entry charge for accompanied children
+	*/
 	protected int calculateAccompaniedChildrenCost(int count) {
 		return count * ACCOMPANIED_CHILDREN_COST;
 	}
 
+	/**
+	* Calculates the costs of unaccompanied children
+	*
+	* @param  count			number of children
+	* 
+	* @return entry charge for unaccompanied children
+	*/
 	protected int calculateUnaccompaniedChildrenCost(int count) {
 		return count * UNACCOMPANIED_CHILDREN_COST;
 	}
 	
+	/**
+	* Calculates the costs of adults
+	*
+	* @param  count			number of adults
+	*
+	* @return entry charge for adults
+	*/
 	protected int calculateAdultCost(int count) {
 		return count * ADULT_COST;
 	}
 	
+	/**
+	* Calculates the costs of seniors
+	*
+	* @param  count			number of seniors
+	*
+	* @return entry charge for seniors
+	*/
 	protected int calculateSeniorCost(int count) {
 		return count * SENIOR_COST;
 	}
 	
+	
+	/**
+	* Computes the number of accompanied children
+	*
+	* @param  childern			number of children
+    * @param  adults            number of adults
+    * @param  seniors			number of seniors
+	*
+	* @return number of accompanied children
+	*/
 	protected int getNumberOfAccompaniedChildren(int children, int adults, int seniors) {
-		return children > adults + seniors ? children - adults - seniors : children;
+		if(children == 0) return 0;
+		
+		int guardians = adults + seniors;
+		
+		return children > guardians ? children - guardians : children;
 	}
 	
+	/**
+	* Computes the number of unaccompanied children
+	*
+	* @param  childern			number of children
+    * @param  adults            number of adults
+    * @param  seniors			number of seniors
+	*
+	* @return number of unaccompanied children
+	*/
 	protected int getNumberOfUnaccompaniedChildren(int children, int adults, int seniors) {
 		return children - getNumberOfAccompaniedChildren(children, adults, seniors);
 	}
