1df8101410a4109a130b7846b8b6d4eba6286e2e
Roshan Kharel <rosn.khrl@gmail.com>
2020-03-16 13:19:00 +1100

    A1 - local

--- 1 assessmest/src/main/ABCBank.java
+++ 1 assessmest/src/main/ABCBank.java
@@ -3,210 +3,205 @@ package main;
 import java.util.Scanner;
 
 /**
-* The class provides a command line interface to ABCBank employees
-* to calculate interest and total balance of a client for
-* a given deposit in fixed terms
-*
-* @version 0.1
-* @author Roshan Kharel, 11691041
-*/
+ * The class provides a command line interface to ABCBank employees to calculate
+ * interest and total balance of a client for a given deposit in fixed terms
+ *
+ * @version 0.1
+ * @author Roshan Kharel, 11691041
+ */
 public class ABCBank {
-	protected Scanner keyboard;
+    protected Scanner keyboard;
 
-	public static void main(String[] args) {
-		new ABCBank().ready();
-	}
-	
-	public ABCBank() {
-		keyboard = new Scanner(System.in);
-	}
+    public static void main(String[] args) {
+        new ABCBank().ready();
+    }
 
-	/**
-    * Method to allow user to calculate multiple clients' interest interactively
-    */
-	public void ready() {
-		while (true) {
-			run();
+    public ABCBank() {
+        keyboard = new Scanner(System.in);
+    }
 
-			System.out.print("\nCalculate interest for another client? [Yes (Y or Yes) / No (any key)]: ");
+    /**
+     * Method to allow user to calculate multiple clients' interest interactively
+     */
+    public void ready() {
+        while (true) {
+            run();
 
-			String keepGoing = keyboard.nextLine().strip().toLowerCase();
+            System.out.print("\nCalculate interest for another client? [Yes (Y or Yes) / No (any key)]: ");
 
-			System.out.println();
+            String keepGoing = keyboard.nextLine().strip().toLowerCase();
 
-			if (keepGoing.equals("yes") || keepGoing.equals("y"))
-				continue;
+            System.out.println();
 
-			break;
-		}
+            if (keepGoing.equals("yes") || keepGoing.equals("y"))
+                continue;
 
-		keyboard.close();
-	}
+            break;
+        }
 
-	/**
-    * Method to allow user to calculate single client's interest interactively
-    */
-	public void run() {
-		String horizontalDashedLine = "-".repeat(50);
+        keyboard.close();
+    }
 
-		System.out.printf(
-				horizontalDashedLine + "\n"
-				+ centerText("ABCBank", 50) + "\n"
-				+ centerText("Enter client's details to calulate the intrest.", 50) + "\n"
-				+ horizontalDashedLine + "\n\n"
-		);
+    /**
+     * Method to allow user to calculate single client's interest interactively
+     */
+    public void run() {
+        String horizontalDashedLine = "-".repeat(50);
 
-		String clientName = askClientName();
-		double depositAmount = askdepositAmount();
-		int terms = askNumberOfTerms();
+        System.out.printf(
+            horizontalDashedLine + "\n"
+            + centerText("ABCBank", 50) + "\n"
+            + centerText("Enter client's details to calulate the intrest.", 50)
+            + "\n" + horizontalDashedLine + "\n\n"
+        );
 
-		deposit(clientName, terms, depositAmount);
-	}
+        String clientName = askClientName();
+        double depositAmount = askdepositAmount();
+        int terms = askNumberOfTerms();
 
-	/**
-	* Interactively prompts for client's name
-	*
-	* @return Client's name
-	*/
-	protected String askClientName() {
-		do {
-			System.out.printf("Enter client's name: ");
-			String clientName = keyboard.nextLine();
+        deposit(clientName, terms, depositAmount);
+    }
 
-			if (clientName.strip().length() > 0) {
-				System.out.println();
-				
-				return clientName;
-			}
+    /**
+     * Interactively prompts for client's name
+     *
+     * @return Client's name
+     */
+    protected String askClientName() {
+        do {
+            System.out.printf("Enter client's name: ");
+            String clientName = keyboard.nextLine();
 
-			System.out.println("Error: Client's name should be at least single character.\n");
-		} while (true);
-	}
+            if (clientName.strip().length() > 0) {
+                System.out.println();
 
-	/**
-	* Interactively prompts for deposit amount
-	*
-	* @return Client's deposit amount
-	*/
-	protected double askdepositAmount() {
-		do {
-			System.out.printf("Enter deposit amount: ");
+                return clientName;
+            }
 
-			try {
-				double depositAmount = Double.valueOf(keyboard.nextLine());
+            System.out.println("Error: Client's name should be at least single character.\n");
+        } while (true);
+    }
 
-				if (depositAmount > 0) {
-					System.out.println();					
-					
-					return depositAmount;
-				}
+    /**
+     * Interactively prompts for deposit amount
+     *
+     * @return Client's deposit amount
+     */
+    protected double askdepositAmount() {
+        do {
+            System.out.printf("Enter deposit amount: ");
 
-				System.out.println("Error: deposit amount should be a greater than 0 (zero).\n");
-			} catch (NumberFormatException e) {
-				System.out.println("Error: deposit amount should be a valid number.\n");
-			}
-		} while (true);
-	}
+            try {
+                double depositAmount = Double.valueOf(keyboard.nextLine());
 
-	/**
-	* Interactively prompts for number of terms in months
-	*
-	* @return Number of terms (in month)
-	*/
-	protected int askNumberOfTerms() {
-		do {
-			System.out.print("Enter terms (in months): ");
+                if (depositAmount > 0) {
+                    System.out.println();
 
-			try {
-				int terms = Integer.valueOf(keyboard.nextLine());
+                    return depositAmount;
+                }
 
-				if (terms > 0) {
-					System.out.println();
-					
-					return terms;
-				}
+                System.out.println("Error: deposit amount should be a greater than 0 (zero).\n");
+            } catch (NumberFormatException e) {
+                System.out.println("Error: deposit amount should be a valid number.\n");
+            }
+        } while (true);
+    }
 
-				System.out.println("Error: Terms must be greater than 0 (zero).\n");
-			} catch (NumberFormatException e) {
-				System.out.println("Error: Terms must be a valid integer.\n");
-			}
-		} while (true);
-	}
+    /**
+     * Interactively prompts for number of terms in months
+     *
+     * @return Number of terms (in month)
+     */
+    protected int askNumberOfTerms() {
+        do {
+            System.out.print("Enter terms (in months): ");
 
-	/**
-    * Calculate and display interest earned, initial deposit amount,
-    * final balance, and other details
-    *
-    * @param  clientName			the name of the client
-    * @param  terms						number of months to deposit
-    * @param  depositAmount		client's deposit amount
-    *
-		* @return	ABCBank instance (for method chaining)
-    */
-	public ABCBank deposit(String clientName, int terms, double depositAmount) {
-		double interest = calculateInterest(depositAmount, terms);
-		String horizontalDashedLine = "-".repeat(50);
+            try {
+                int terms = Integer.valueOf(keyboard.nextLine());
 
-		double finalBalance = depositAmount + interest;
+                if (terms > 0) {
+                    System.out.println();
 
-		System.out.printf(
-				horizontalDashedLine + "\n"
-				+ centerText("ABCBank", 50) + "\n"
-				+ centerText("Return on Bank Term Deposits", 50) + "\n"
-				+ horizontalDashedLine + "\n\n"
-				+ "Client: %s\n"
-				+ "Deposit Amount: $%.2f\n"
-				+ "Term: %s Month" + (terms > 1 ? "s" : "") + "\n"
-				+ "Interest Earned: $%.2f\n"
-				+ "Final Balance: $%.2f\n\n"
-				+ horizontalDashedLine + "\n",
-			clientName, depositAmount, terms, interest, finalBalance);
+                    return terms;
+                }
 
-		return this;
-	}
+                System.out.println("Error: Terms must be greater than 0 (zero).\n");
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Terms must be a valid integer.\n");
+            }
+        } while (true);
+    }
 
-	/**
-    * Calculate interest that the client can earn at the end of terms of deposit
-    *
-    * @param  amount	the client's deposit amount
-    * @param  terms		duration (in months) of fixed deposit
-    *
-		* @return	interest earned by client
-    */
-	protected double calculateInterest(double amount, int terms) {
-		double interest = 0.0;
-		double years = Double.valueOf(terms) / 12d;
+    /**
+     * Calculate and display interest earned, initial deposit amount, final balance,
+     * and other details
+     *
+     * @param clientName    the name of the client
+     * @param terms         number of months to deposit
+     * @param depositAmount client's deposit amount
+     *
+     * @return ABCBank instance (for method chaining)
+     */
+    public ABCBank deposit(String clientName, int terms, double depositAmount) {
+        double interest = calculateInterest(depositAmount, terms);
+        String horizontalDashedLine = "-".repeat(50);
 
-		if (amount <= 1000) {
-			interest = amount * 0.02;
-		} else if (amount > 1000 && amount <= 5000) {
-			interest = amount * 0.025;
-		} else if (amount > 5000 && amount <= 10000) {
-			interest = amount * 0.03;
-		} else if (amount > 10000 && amount <= 20000) {
-			interest = amount * 0.035;
-		} else {
-			interest = amount * 0.04;
-		}
+        double finalBalance = depositAmount + interest;
 
-		return interest * years;
-	}
+        System.out.printf(
+                horizontalDashedLine + "\n"
+                + centerText("ABCBank", 50) + "\n"
+                + centerText("Return on Bank Term Deposits", 50)
+                + "\n" + horizontalDashedLine + "\n\n"
+                    + "Client: %s\n"
+                    + "Deposit Amount: $%.2f\n"
+                    + "Term: %s Month" + (terms > 1 ? "s" : "") + "\n"
+                    + "Interest Earned: $%.2f\n"
+                    + "Final Balance: $%.2f\n\n"
+                + horizontalDashedLine + "\n",
+            clientName, depositAmount, terms, interest, finalBalance
+        );
 
-	/**
-	* Utility method to center a given text by adding whitespace
-	*
-	* @param text		the text to be padded with whitespace
-	* @param width	the length of the characters in which text is to be centered
-	*
-	* @return 			text padded with whitespace
-	*/
-	protected String centerText(String text, int width) {
-		return String.format(
-			"%-" + width + "s",
-			String.format(
-				"%" + (text.length() + (width - text.length()) / 2) + "s",
-				text
-			)
-		);
-	}
+        return this;
+    }
+
+    /**
+     * Calculate interest that the client can earn at the end of terms of deposit
+     *
+     * @param amount the client's deposit amount
+     * @param terms  duration (in months) of fixed deposit
+     *
+     * @return interest earned by client
+     */
+    protected double calculateInterest(double amount, int terms) {
+        double interest = 0.0;
+        double years = Double.valueOf(terms) / 12d;
+
+        if (amount <= 1000) {
+            interest = amount * 0.02;
+        } else if (amount > 1000 && amount <= 5000) {
+            interest = amount * 0.025;
+        } else if (amount > 5000 && amount <= 10000) {
+            interest = amount * 0.03;
+        } else if (amount > 10000 && amount <= 20000) {
+            interest = amount * 0.035;
+        } else {
+            interest = amount * 0.04;
+        }
+
+        return interest * years;
+    }
+
+    /**
+     * Utility method to center a given text by adding whitespace
+     *
+     * @param text  the text to be padded with whitespace
+     * @param width the length of the characters in which text is to be centered
+     *
+     * @return text padded with whitespace
+     */
+    protected String centerText(String text, int width) {
+        return String.format("%-" + width + "s",
+                String.format("%" + (text.length() + (width - text.length()) / 2) + "s", text));
+    }
 }
--- 1 assessmest/src/main/ZooTicket.java
+++ 1 assessmest/src/main/ZooTicket.java
@@ -4,205 +4,195 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ZooTicket {
-	protected final int ACCOMPANIED_CHILDREN_COST = 2;
-	protected final int UNACCOMPANIED_CHILDREN_COST = 5;
-	protected final int ADULT_COST = 10;
-	protected final int SENIOR_COST = 8;
-	
-	protected int totalCharge = 0;
-	Scanner keyboard;
+    protected final int ACCOMPANIED_CHILDREN_COST   = 2;
+    protected final int UNACCOMPANIED_CHILDREN_COST = 5;
+    protected final int ADULT_COST                  = 10;
+    protected final int SENIOR_COST                 = 8;
 
-	public static void main(String[] args) {
-		new ZooTicket().run();
-	}
-	
-	public ZooTicket() {
-		keyboard = new Scanner(System.in);
-	}
-	
-	public void run() {
-		boolean keepGoing = true;
-		
-		ArrayList<Integer> acceptedValues = new ArrayList<Integer>();
-		acceptedValues.add(0);
-		acceptedValues.add(1);
-		
-		
-		String horizontalDashedLine = "-".repeat(60);
-		
-		System.out.printf(
-				horizontalDashedLine + "\n" +
-				centerText("Welocme to the Zoo", 60) + "\n" +
-				centerText("Our ticketing price is as follows:", 60) + "\n\n" +
-				"Children 5 years old and younger: free\n" + 
-				"Accompanied children from 6 to 15 years old: $2 each\n" + 
-				"Unaccompanied children from 6 to 15 years old: $5 each\n" + 
-				"Adults from 16 to 59 years old: $10 each\n" + 
-				"Seniors from 60 years and older: $8 each\n\n" +
-				horizontalDashedLine + "\n\n"
-		);
-		
-		do {
-			System.out.print("Enter a group? (Yes=1/no=0): ");
-			int group;
-			
-			try {
-				group = Integer.valueOf(keyboard.nextLine());
-				
-				if(!acceptedValues.contains(group)) {
-					System.out.println("Error: only 1 and 0 are accepted values.\n");
-					continue;
-				}
-				
-				System.out.println();
-			} catch (NumberFormatException e) {
-				System.out.println("Error: only 1 and 0 are accepted values.\n");
-				continue;
-			}
-			
-			if(group == 0) {
-				keepGoing = false;
-				showTotal();
-				break;
-			}
-			
-			int numberOfChildren = askNumberOfChildren();
-			int numberOfAdults = askNumberOfAdults();
-			int numberOfSeniors = askNumberOfSeniors();
-			
-			showTotal(numberOfChildren, numberOfAdults, numberOfSeniors);
-		} while(keepGoing);
-		
-		keyboard.close();
-		
-	}
-	
-	protected int askNumberOfChildren() {
-		do {
-			System.out.print("Enter the number of children (age 6–15): ");
-			int numberOfChildren;
-			
-			try {
-				numberOfChildren = Integer.valueOf(keyboard.nextLine());
-				
-				if(numberOfChildren < 0) {
-					System.out.println("Error: The value cannot be negative.\n");
-				}
-				
-				System.out.println();
-				
-				return numberOfChildren;
-			} catch(NumberFormatException e) {
-				System.out.println("Error: Only integer value is accepted.\n");
-			}
-			
-		} while(true);
-	}
-	
-	protected int askNumberOfSeniors() {
-		do {
-			System.out.print("Enter the number of seniors (age 60+): ");
-			int numberOfChildren;
-			
-			try {
-				numberOfChildren = Integer.valueOf(keyboard.nextLine());
-				
-				if(numberOfChildren < 0) {
-					System.out.println("Error: The value cannot be negative.\n");
-				}
-				
-				System.out.println();
-				
-				return numberOfChildren;
-			} catch(NumberFormatException e) {
-				System.out.println("Error: Only integer value is accepted.\n");
-			}
-			
-		} while(true);
-	}
-	
-	protected int askNumberOfAdults() {
-		do {
-			System.out.print("Enter the number of adults (age 16–59): ");
-			int numberOfChildren;
-			
-			try {
-				numberOfChildren = Integer.valueOf(keyboard.nextLine());
-				
-				if(numberOfChildren < 0) {
-					System.out.println("Error: The value cannot be negative.\n");
-				}
-				
-				System.out.println();
-				
-				return numberOfChildren;
-			} catch(NumberFormatException e) {
-				System.out.println("Error: Only integer value is accepted.\n");
-			}
-			
-		} while(true);
-	}
-	
-	protected void showTotal() {
-		System.out.printf("Total takings: $%d\n", totalCharge);
-	}
-	
-	
-	protected void showTotal(int children, int adults, int seniors) {
-		int numberOfAccompaniedChildren = getNumberOfAccompaniedChildren(children, adults, seniors);
-		int numberOfUnaccompaniedChildren = getNumberOfUnaccompaniedChildren(children, adults, seniors);
-		
-		int accompaniedChildrenCost = calculateAccompaniedChildrenCost(numberOfAccompaniedChildren);
-		int unacompaniedChildrenCost = calculateUnaccompaniedChildrenCost(numberOfUnaccompaniedChildren);
-		int adultCost = calculateAdultCost(adults);
-		int seniorCost = calculateSeniorCost(seniors);
-		
-		int groupCharge = accompaniedChildrenCost + unacompaniedChildrenCost + adultCost + seniorCost;
-		
-		totalCharge += groupCharge;
-		
-		System.out.printf("Total entry charge is $%d\n\n", groupCharge);
-	}
-	
-	protected int calculateAccompaniedChildrenCost(int count) {
-		return count * ACCOMPANIED_CHILDREN_COST;
-	}
+    protected int       totalCharge                 = 0;
+    protected Scanner   keyboard;
 
-	protected int calculateUnaccompaniedChildrenCost(int count) {
-		return count * UNACCOMPANIED_CHILDREN_COST;
-	}
-	
-	protected int calculateAdultCost(int count) {
-		return count * ADULT_COST;
-	}
-	
-	protected int calculateSeniorCost(int count) {
-		return count * SENIOR_COST;
-	}
-	
-	protected int getNumberOfAccompaniedChildren(int children, int adults, int seniors) {
-		return children > adults + seniors ? children - adults - seniors : children;
-	}
-	
-	protected int getNumberOfUnaccompaniedChildren(int children, int adults, int seniors) {
-		return children - getNumberOfAccompaniedChildren(children, adults, seniors);
-	}
-	
-	/**
-	* Utility method to center a given text by adding whitespace
-	*
-	* @param text		the text to be padded with whitespace
-	* @param width      the length of the characters in which text is to be centered
-	*
-	* @return 			text padded with whitespace
-	*/
-	private String centerText(String text, int width) {
-		return String.format(
-			"%-" + width + "s",
-			String.format(
-				"%" + (text.length() + (width - text.length()) / 2) + "s",
-				text
-			)
-		);
-	}
+    public static void main(String[] args) {
+        new ZooTicket().run();
+    }
+
+    public ZooTicket() {
+        keyboard = new Scanner(System.in);
+    }
+
+    public void run() {
+        boolean keepGoing = true;
+
+        ArrayList<Integer> acceptedValues = new ArrayList<Integer>();
+        acceptedValues.add(0);
+        acceptedValues.add(1);
+
+        String horizontalDashedLine = "-".repeat(60);
+
+        System.out.printf(
+                horizontalDashedLine + "\n"
+                + centerText("Welocme to the Zoo", 60) + "\n"
+                + centerText("Our ticketing price is as follows:", 60) + "\n\n"
+                + "Children 5 years old and younger: free\n" + "Accompanied children from 6 to 15 years old: $2 each\n"
+                + "Unaccompanied children from 6 to 15 years old: $5 each\n"
+                + "Adults from 16 to 59 years old: $10 each\n" + "Seniors from 60 years and older: $8 each\n\n"
+                + horizontalDashedLine + "\n\n");
+
+        do {
+            System.out.print("Enter a group? (Yes=1/no=0): ");
+            int group;
+
+            try {
+                group = Integer.valueOf(keyboard.nextLine());
+
+                if (!acceptedValues.contains(group)) {
+                    System.out.println("Error: only 1 and 0 are accepted values.\n");
+                    continue;
+                }
+
+                System.out.println();
+            } catch (NumberFormatException e) {
+                System.out.println("Error: only 1 and 0 are accepted values.\n");
+                continue;
+            }
+
+            if (group == 0) {
+                keepGoing = false;
+                showTotal();
+                break;
+            }
+
+            int numberOfChildren = askNumberOfChildren();
+            int numberOfAdults = askNumberOfAdults();
+            int numberOfSeniors = askNumberOfSeniors();
+
+            showTotal(numberOfChildren, numberOfAdults, numberOfSeniors);
+        } while (keepGoing);
+
+        keyboard.close();
+
+    }
+
+    protected int askNumberOfChildren() {
+        do {
+            System.out.print("Enter the number of children (age 6–15): ");
+            int numberOfChildren;
+
+            try {
+                numberOfChildren = Integer.valueOf(keyboard.nextLine());
+
+                if (numberOfChildren < 0) {
+                    System.out.println("Error: The value cannot be negative.\n");
+                }
+
+                System.out.println();
+
+                return numberOfChildren;
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Only integer value is accepted.\n");
+            }
+
+        } while (true);
+    }
+
+    protected int askNumberOfSeniors() {
+        do {
+            System.out.print("Enter the number of seniors (age 60+): ");
+            int numberOfChildren;
+
+            try {
+                numberOfChildren = Integer.valueOf(keyboard.nextLine());
+
+                if (numberOfChildren < 0) {
+                    System.out.println("Error: The value cannot be negative.\n");
+                }
+
+                System.out.println();
+
+                return numberOfChildren;
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Only integer value is accepted.\n");
+            }
+
+        } while (true);
+    }
+
+    protected int askNumberOfAdults() {
+        do {
+            System.out.print("Enter the number of adults (age 16–59): ");
+            int numberOfChildren;
+
+            try {
+                numberOfChildren = Integer.valueOf(keyboard.nextLine());
+
+                if (numberOfChildren < 0) {
+                    System.out.println("Error: The value cannot be negative.\n");
+                }
+
+                System.out.println();
+
+                return numberOfChildren;
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Only integer value is accepted.\n");
+            }
+
+        } while (true);
+    }
+
+    protected void showTotal() {
+        System.out.printf("Total takings: $%d\n", totalCharge);
+    }
+
+    protected void showTotal(int children, int adults, int seniors) {
+        int numberOfAccompaniedChildren = getNumberOfAccompaniedChildren(children, adults, seniors);
+        int numberOfUnaccompaniedChildren = getNumberOfUnaccompaniedChildren(children, adults, seniors);
+
+        int accompaniedChildrenCost = calculateAccompaniedChildrenCost(numberOfAccompaniedChildren);
+        int unacompaniedChildrenCost = calculateUnaccompaniedChildrenCost(numberOfUnaccompaniedChildren);
+        int adultCost = calculateAdultCost(adults);
+        int seniorCost = calculateSeniorCost(seniors);
+
+        int groupCharge = accompaniedChildrenCost + unacompaniedChildrenCost + adultCost + seniorCost;
+
+        totalCharge += groupCharge;
+
+        System.out.printf("Total entry charge is $%d\n\n", groupCharge);
+    }
+
+    protected int calculateAccompaniedChildrenCost(int count) {
+        return count * ACCOMPANIED_CHILDREN_COST;
+    }
+
+    protected int calculateUnaccompaniedChildrenCost(int count) {
+        return count * UNACCOMPANIED_CHILDREN_COST;
+    }
+
+    protected int calculateAdultCost(int count) {
+        return count * ADULT_COST;
+    }
+
+    protected int calculateSeniorCost(int count) {
+        return count * SENIOR_COST;
+    }
+
+    protected int getNumberOfAccompaniedChildren(int children, int adults, int seniors) {
+        return children > adults + seniors ? children - adults - seniors : children;
+    }
+
+    protected int getNumberOfUnaccompaniedChildren(int children, int adults, int seniors) {
+        return children - getNumberOfAccompaniedChildren(children, adults, seniors);
+    }
+
+    /**
+     * Utility method to center a given text by adding whitespace
+     *
+     * @param text  the text to be padded with whitespace
+     * @param width the length of the characters in which text is to be centered
+     *
+     * @return text padded with whitespace
+     */
+    private String centerText(String text, int width) {
+        return String.format("%-" + width + "s",
+                String.format("%" + (text.length() + (width - text.length()) / 2) + "s", text));
+    }
 }
